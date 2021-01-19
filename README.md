652c18b605d97c9de948af26894c30922612d641
Ammad <ammad.ahmed.eng@gmail.com>
2021-01-19 23:48:53 +0100

    100% branch coverage implemented

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # CodingExample_Test_Memoization
 
+The flow of the code is explained with the help of the comments 
+in each file which are memoization.py and test.py
+
+Loggin resuls contain the Failed and Passed initial test case.
+Furthemore, it consists the Branch Coverage Pass-Fail test cases for the code which finally stands
 LoggingResults : /logMemoization/
 
+Unit Testing Framework
 Framework : pytest
 
+BDD plugins for pytest framework
 Plugins : describe-it-0.1.0
--- test.py
+++ test.py
@@ -12,6 +12,7 @@ import memoization
 # Postional Argument (memoizeValue) with lambda anonymous function
 varMemoizeValue = lambda memoizeValue: memoizeValue
 
+
 # memoization-branch-coverage class for controlling memoize Test Plan (TP)
 @pytest.mark.describe('memoization-branch-coverage')
 class Test_memoization_branch_cov:
@@ -23,20 +24,24 @@ class Test_memoization_branch_cov:
     # Test Cases* (TC*) : One test case = One Branch Coverage
 
     # The total test coverage for TS1 is:
-    # (Tested Branch / Total Branch) * 100 =
+    # (Tested Branch / Total Branch) * 100 = 14 / 14 * 100 = 100% Branch Coverage
     ###############################################################################
 
     # Function for memoize value
     global varMemoizeValue
 
-    @pytest.mark.it('Test Should Passed - Ensure to cover the path when resolver is not None. Parameters: (1, 100, 1000)')
+    @pytest.mark.it('Test Should Passed - Ensure to cover the branch when resolver is not None. '
+                    'It will cover branch of Line 26 (if), Line 20 (else), Line 55 (else) and Line 59 (if).'
+                    'Parameters: (1, 100, 1000)')
     def test_TS1_TC1(self):
         self.varReturnValue = 1
 
         # Assertion for checking return value, it should passed
         assert memoization.memoize(varMemoizeValue(self.varReturnValue), 100, 1000) == 1
 
-    @pytest.mark.it('Test Should Failed - Their is no expiry of old value, so it should failed. Parameters: (2, 100, 1000)')
+    @pytest.mark.it('Test Should Failed - Their is no expiry of old value, so it should failed. '
+                    'It will cover remaining branch coverage of Line 59 conditions (if-elif- (else)).'
+                    'Parameters: (2, 100, 1000)')
     def test_TS1_TC2(self):
         self.varReturnValue = 2
 
@@ -44,9 +49,9 @@ class Test_memoization_branch_cov:
         # thus failing the result
         assert memoization.memoize(varMemoizeValue(self.varReturnValue), 100, 1000) == 2
 
-
-    @pytest.mark.it('Test Should Passed - Ensure to cover the path when resolver is not None and memoize value timed '
-                    'out. Also pass tuples to cover statement coverage of function resolver(). '
+    @pytest.mark.it('Test Should Passed - Ensure to cover the branch when resolver is not None and '
+                    'memoize value timed out. Also pass tuples to cover branch coverage of function resolver().'
+                    'It will cover remaining branch of Line 18 (if), Line 66 (elif).'
                     'Parameters: (2, (100, 101), 1000)')
     def test_TS1_TC3(self):
         self.varReturnValue = 2
@@ -56,29 +61,36 @@ class Test_memoization_branch_cov:
             # thus passing the result
             assert memoization.memoize(varMemoizeValue(self.varReturnValue), (100, 101), 1000) == 2
 
-    @pytest.mark.it('Test Should Passed - Ensure to cover the path when resolver is None. Parameters: (11, None, 1000)')
+    @pytest.mark.it('Test Should Passed - Ensure to cover the branch when resolver is None. '
+                    'It will cover remaining branch of Line 26 (False condition), Line 35 (if), Line 39 (if).'
+                    'Parameters: (11, None, 1000)')
     def test_TS1_TC4(self):
         self.varReturnValue = 11
 
         # Assertion for checking return value, it should passed
         assert memoization.memoize(varMemoizeValue(self.varReturnValue), None, 1000) == 11
 
-    @pytest.mark.it('Test Should Failed - Their is no expiry of old value, so it should failed. Parameters: (11, None, 1000)')
+    @pytest.mark.it('Test Should Passed - Although, their is no expiry of old value, but without resolver key,'
+                    'the value will just be added in the List. '
+                    'It will cover remaining branch of Line 39 conditions (if-elif- (else)).'
+                    'Parameters: (11, None, 1000)')
     def test_TS1_TC5(self):
         self.varReturnValue = 11
 
-        # Assertion for checking new value, but it will returned old value
-        # thus failing the result
+        # Assertion for checking new value, but it will returned old value but the test will be passed
+        # because their is no resolver to match the Key in correspondence with the memoize value
         assert memoization.memoize(varMemoizeValue(self.varReturnValue), None, 1000) == 11
 
-    @pytest.mark.it('Test Should Passed - Ensure to cover the path when resolver is None and memoize value timed out. Parameters: (12, None, 1000)')
+    @pytest.mark.it('Test Should Passed - Ensure to cover the branch when resolver is None and memoize value timed out.'
+                    'It will cover remaining branch of Line 46 (elif).'
+                    'Parameters: (11, None, 1000)')
     def test_TS1_TC6(self):
-        self.varReturnValue = 12
+        self.varReturnValue = 11
 
         with freeze_time(datetime.datetime.now() + datetime.timedelta(milliseconds=1001)):
             # Assertion for checking new value, it will returned new value because old value expire
             # thus passing the result
-            assert memoization.memoize(varMemoizeValue(self.varReturnValue), None, 1000) == 12
+            assert memoization.memoize(varMemoizeValue(self.varReturnValue), None, 1000) == 11
 
 
 if __name__ == '__main__':
